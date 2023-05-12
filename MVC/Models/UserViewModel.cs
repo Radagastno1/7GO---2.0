ca8677b5f3f75392b058a9def689f23245cfb1d2
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-05-12 12:56:17 +0200

    bli medlem och logga in knapp funkar

--- MVC/Models/UserViewModel.cs
+++ MVC/Models/UserViewModel.cs
@@ -4,6 +4,8 @@ public class UserViewModel
 {
     public int Id{get;set;}
     public string Name{get;set;} = "Angelina";
+    public string PhoneNumber{get;set;}
+    public string Email{get;set;}
     public int Age{get;set;} = 30;
     public string UserInlog{get;set;}
     public List<CORE.Entities.Challenge> AvailableChallenges{get;set;}
